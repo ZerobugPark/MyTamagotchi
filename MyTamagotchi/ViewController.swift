de21fe72f3f3dfeb43128da8fc358f22947bcddf
young <zerobugpark@gmail.com>
2025-02-21 00:44:44 +0900

    Initial Commit

---
+++ MyTamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTamagotchi
+//
+//  Created by youngkyun park on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
