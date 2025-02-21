df2c913f9dd9084d6ecbcfb88cb7d96d0da168e2
young <zerobugpark@gmail.com>
2025-02-22 03:38:04 +0900

    [파일추가]
    DetailSetting 파일 추가

---
+++ MyTamagotchi/DetailSetting/DetailSettingView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailSettingView.swift
+//  MyTamagotchi
+//
+//  Created by youngkyun park on 2/22/25.
+//
+
+import Foundation
---
+++ MyTamagotchi/DetailSetting/DetailSettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailSettingViewController.swift
+//  MyTamagotchi
+//
+//  Created by youngkyun park on 2/22/25.
+//
+
+import UIKit
+
+class DetailSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+ 
+
+}
